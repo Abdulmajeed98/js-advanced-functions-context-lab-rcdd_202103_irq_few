eb2fc265421ee1b6d5af72d22e08e16b6516fb8f
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-06-13 14:14:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,48 +16,59 @@ let createEmployeeRecords = function(employeeRowData) {
     })
 }
 
-let createTimeInEvent = function(employee, dateStamp){
+let createTimeInEvent = function(dateStamp){
     let [date, hour] = dateStamp.split(' ')
 
-    employee.timeInEvents.push({
+    this.timeInEvents.push({
         type: "TimeIn",
         hour: parseInt(hour, 10),
         date,
     })
 
-    return employee
+    return this
 }
 
-let createTimeOutEvent = function(employee, dateStamp){
+let createTimeOutEvent = function(dateStamp){
     let [date, hour] = dateStamp.split(' ')
 
-    employee.timeOutEvents.push({
+    this.timeOutEvents.push({
         type: "TimeOut",
         hour: parseInt(hour, 10),
         date,
     })
 
-    return employee
+    return this
 }
 
-let hoursWorkedOnDate = function(employee, soughtDate){
-    let inEvent = employee.timeInEvents.find(function(e){
+let hoursWorkedOnDate = function(soughtDate){
+    let inEvent = this.timeInEvents.find(function(e){
         return e.date === soughtDate
     })
 
-    let outEvent = employee.timeOutEvents.find(function(e){
+    let outEvent = this.timeOutEvents.find(function(e){
         return e.date === soughtDate
     })
 
     return (outEvent.hour - inEvent.hour) / 100
 }
 
-let wagesEarnedOnDate = function(employee, dateSought){
-    let rawWage = hoursWorkedOnDate(employee, dateSought)
-        * employee.payPerHour
+let wagesEarnedOnDate = function(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought)
+        * this.payPerHour
     return parseFloat(rawWage.toString())
 }
 
+let allWagesFor = function(){
+    let eligibleDates = this.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0)
+
+    return payable
+}
 
 let findEmployeeByFirstName = function(srcArray, firstName) {
   return srcArray.find(function(rec){
@@ -67,7 +78,7 @@ let findEmployeeByFirstName = function(srcArray, firstName) {
 
 let calculatePayroll = function(arrayOfEmployeeRecords){
     return arrayOfEmployeeRecords.reduce(function(memo, rec){
-        return memo + allWagesFor(rec)
+        return memo + allWagesFor.call(rec)
     }, 0)
 }
 /*
@@ -78,14 +89,3 @@ let calculatePayroll = function(arrayOfEmployeeRecords){
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
-let allWagesFor = function(employee){
-    let eligibleDates = employee.timeInEvents.map(function(e){
-        return e.date
-    })
-
-    let payable = eligibleDates.reduce(function(memo, d){
-        return memo + wagesEarnedOnDate(employee, d)
-    }, 0)
-
-    return payable
-}
